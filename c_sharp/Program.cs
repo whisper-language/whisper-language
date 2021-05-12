206071a3e84e9ed33ba82ae4195f34670a1c3471
jingdor <945627077@qq.com>
2021-05-12 09:30:45 +0800

    '修改c_sharpbug'

--- c_sharp/Program.cs
+++ c_sharp/Program.cs
@@ -2,26 +2,47 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http.Headers;
+using whisper_language.gen;
 
 namespace whisper_language
 {
+
+    class sum : BuildInFunction
+    {
+        public TLValue invoke(List<TLValue> argv)
+        {
+            int s = 0;
+            argv.ForEach(e =>
+            {
+                s += e.asInt();
+            });
+            return new TLValue(s);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            try {
-                       var a = CharStreams.fromPath("../test.pg");
-                       var lexer = new TLLexer(a);
-                       TLParser parser = new TLParser(new CommonTokenStream(lexer));
-                       parser.BuildParseTree=true;
-                       var tree = parser.parse();
-                       Scope scope = new Scope(null,false);
-                       var visitor = new EvalVisitor(scope,new Dictionary<string, Function>(), new Dictionary<string, BuildInFunction>());
+            try
+            {
+                var a = CharStreams.fromPath("../test.pg");
+                var lexer = new TLLexer(a);
+                TLParser parser = new TLParser(new CommonTokenStream(lexer));
+                parser.BuildParseTree = true;
+                var tree = parser.parse();
+                Scope scope = new Scope(null, false);
+
+                var funcs = new Dictionary<string, BuildInFunction>();
+                funcs.Add("@sum", new sum());
+                var visitor = new EvalVisitor(scope, new Dictionary<string, Function>(), funcs);
                 visitor.Visit(tree);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 Console.WriteLine(e.Message);
                 Console.WriteLine(e.StackTrace.ToString());
-                   }
+            }
         }
     }
 }
